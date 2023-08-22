9d2560709dc1890cb1134f388334e03f7a64e2bc
KrieqqeR <burakcevizli@gmail.com>
2023-08-22 20:26:02 +0300

    güncel

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,19 @@
 import React from "react";
+import {differenceInDays,formatDistanceToNow} from 'date-fns'
 
 const Task = ({ taskObj, onComplete }) => {
+
+  const result = differenceInDays(new Date(taskObj.deadline),new Date()) 
+
+  const resultIki = result > 0 ? `${result} gün sonra !` : formatDistanceToNow(new Date(taskObj.deadline),{includeSeconds:true})
+
+  const color = "bg-"
+   
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className={resultIki<3 ? "bg-sky-600": ""}>{resultIki}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
